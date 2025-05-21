b1129c4acddcff30c52938950c9b13a8838b87f3
Camilo <juachomanco@gmail.com>
2025-05-21 10:24:08 -0500

    primer commit

---
+++ EjemplosOidentificadores.js
@@ -0,0 +1,30 @@
+// let edad = 17;
+
+// // if (edad >= 18) {
+// //     console.log("Eres mayor de edad");
+// // }else {
+// //     console.log("Eres menor de edad");
+// // }
+// //prompt("Presiona enter para continuar"); ventana para ingresar contenido
+// //print("Fin del programa"); imprimir en documento
+// //document.write("Hola"); imprime en documento
+
+// let edad = 17, reserva = 0;
+
+// if (edad >= 18 && reserva == true) {
+//     console.log("Eres mayor de edad y tienes reserva");
+// } else {
+//     console.log("No tienes reserva y aparte eres menor de edad");
+// }
+let edad = 18, reserva = true;
+
+if (edad >= 18 ) {
+    console.log("Eres mayor de edad");
+    if (reserva == true) {
+        console.log("Tienes reserva");
+}else{
+        console.log("No tienes reserva");
+    }
+    }else {
+    console.log("Eres menor de edad");
+}
